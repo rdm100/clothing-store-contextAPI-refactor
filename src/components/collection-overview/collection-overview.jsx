631003b2e97fc2a50cf2bafad36a8e0b2101aff1
rdm100 <robertode_marco@hotmail.com>
2021-03-27 18:47:25 +0000

    updated collection overview component to use collections context

--- src/components/collection-overview/collection-overview.jsx
+++ src/components/collection-overview/collection-overview.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-import { selectCollectionsForPreview } from '../../redux/shop/shop-selector';
+import React, { useContext } from 'react';
+import CollectionsContext from '../../contexts/collections/collections-context';
 import CollectionPreview from '../collection-preview/collection-preview';
 
 import './collection-overview.scss';
 
-const CollectionOverview = ({ collections }) => {
+const CollectionOverview = () => {
+  const collectionsMap = useContext(CollectionsContext);
+  const collections = Object.keys(collectionsMap).map(key => collectionsMap[key]);
   return (
     <div className='collections-overview'>
     {
@@ -17,8 +17,5 @@ const CollectionOverview = ({ collections }) => {
     </div>
   );
 };
-const mapStateToProps = createStructuredSelector ({
-  collections: selectCollectionsForPreview 
-})
 
-export default connect(mapStateToProps, null)(CollectionOverview);
+export default CollectionOverview;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom'; 
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './redux/store';
 import CartProvider from '../src/providers/cart/cart-provider';
 
 import './index.css';
@@ -11,15 +8,11 @@ import App from './App';
 
 ReactDOM.render(
   <CartProvider>
-    <Provider store={store}>
-      <Router>  
-        <PersistGate persistor={persistor}>
-          <React.StrictMode>  
-            <App />
-          </React.StrictMode>
-        </PersistGate>
-      </Router>
-    </Provider>
+    <Router>  
+      <React.StrictMode>  
+        <App />
+      </React.StrictMode>
+    </Router>
   </CartProvider>, 
   document.getElementById('root')
 );
